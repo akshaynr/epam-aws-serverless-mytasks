6b21123217438a942813b0b9ad95b89ee8f26c10
Akshay Raskar <akshay.raskar@coxautoinc.com>
2024-08-04 22:37:20 +0530

    update lambda response

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,12 +1,8 @@
 exports.handler = async (event) => {
     // TODO implement
-    const result = {
-        statusCode: 200,
-        message: "Hello from Lambda"
-    };
     const response = {
         statusCode: 200,
-        body: result,
+        message: "Hello from Lambda",
     };
     return response;
 };
